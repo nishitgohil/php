1b34a2c801b6451699b5cc6b653eb95b568e1fe9
nishit gohil <nishitg512000@gmail.com>
2023-08-13 13:32:49 +0530

    commit

--- float.php
+++ float.php
@@ -3,5 +3,7 @@ $float2 = 1.2e3; // exponential float (1.2 x 10^3)
 $float3 = 0.123456789; // precision float
 
 <!-- example -->
+<?php
 $price = 9.99;
 echo 'The price of the item is: $' . $price;
+?>
